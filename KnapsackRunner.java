1f8a8bf3545db0b34e380333d9fbeb9e63b4bcf4
Bluju <JASahagun26@gmail.com>
2023-10-24 12:16:50 -0500

    add initial structure code

--- KnapsackRunner.java
+++ KnapsackRunner.java
@@ -1,1 +1,29 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class KnapsackRunner{
 
+
+    public static void main(String[] args){
+        File input = new File("input.txt");
+        try{
+            Scanner sc = new Scanner(input);
+            readInputFile();
+            sc.close();
+        } catch(Exception e){
+            System.out.println("Input file not found");
+            return;
+        }
+    }
+
+    private static void readInputFile(){
+        while(true){
+            //read values from input file
+            ArrayList<Integer> values = new ArrayList();
+
+            //read weights from input file
+
+            //read capacity from input file
+        }
+    }
+}
--- KnapsackSolver.java
+++ KnapsackSolver.java
@@ -1,1 +1,32 @@
+import java.util.ArrayList;
+public class KnapsackSolver{
 
+    public KnapsackSolver(){
+        //constructor
+
+    }
+
+    private void dynamicSolver(ArrayList<Integer> values, long[] weights, int capacity){
+        //using dynamic programming solve the knapsack problem
+
+        output();
+    }
+
+    private void dynamicFractionalSolver(){
+
+    }
+
+    private void greedySolver(){
+
+    }
+
+    private void greedyFractionalSolver(){
+
+    }
+
+    private void output(){
+        System.out.println("Total weight: ");
+        System.out.println("Packed items: ");
+        System.out.println("Packed weights: ");
+    }
+}
